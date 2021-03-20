5fbf26e91c700d81beef0556f3c7462c234a7f8d
James Murphy <murphyjj@my.gvltec.edu>
2021-03-20 14:14:24 -0500

    configured client_origin

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  CLIENT_ORIGIN:
+    "https://personal-library-m4avfwxvo-jamesivdeveloper.vercel.app/",
 };
